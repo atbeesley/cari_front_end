4cbf9ada41d57e968334cf86886153459c2a7d19
user <you@exampalex.com>
2019-12-15 14:05:59 +0000

    adds MainContainer

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../NavBar.js'
+// remember to import other stuffs later
+
+const MainContainer = () => {
+
+  return (
+
+    <div>
+      <NavBar />
+    </div>
+  )
+
+}
+
+export default MainContainer
