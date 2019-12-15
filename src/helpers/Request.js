44b121eddd2635cca4eaef527a8a548981df1afb
user <you@exampalex.com>
2019-12-15 15:09:50 +0000

    adds helpers folder and Request

---
+++ src/helpers/Request.js
@@ -0,0 +1,27 @@
+class Request {
+    get(url) {
+      return fetch(url)
+      .then((res) => res.json());
+    }
+    delete(url) {
+      return fetch(url, {
+        method: "DELETE",
+        headers: {'Content-Type': 'application/json'}
+      })
+    }
+    post(url, payload){
+      return fetch(url, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+      })
+    }
+    patch(url, payload){
+      return fetch(url, {
+        method: "PATCH",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+      })
+    }
+}
+export default Request
